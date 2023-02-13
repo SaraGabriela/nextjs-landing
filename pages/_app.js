070274ee08e168ce0508f7421343e790f03aee5f
SaraGabriela <sara.diaz.oporto@gmail.com>
2023-02-13 15:26:35 -0500

    update de landing

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
-import '../styles/globals.css'
+import '../styles/slick.css'
+import '../styles/font-awesome.min.css'
+//import '../styles/LineIcons.css'
+import '../styles/animate.css'
+import '../styles/magnific-popup.css'
+import '../styles/bootstrap.min.css'
+import '../styles/default.css'
+import '../styles/style.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
